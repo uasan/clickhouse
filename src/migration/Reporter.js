b1fb4fba3d8eb75b0057369e8245886a5c977c5b
uasan <ua.san.alex@gmail.com>
2025-08-17 19:40:22 +0200

    Fix reporter

--- src/migration/Reporter.js
+++ src/migration/Reporter.js
@@ -4,21 +4,22 @@ export class Reporter {
   count = 0;
 
   star() {
-    console.log(styleText('green', 'Migrations Start'));
+    console.log(styleText('blueBright', 'ClickHouse ') + styleText('green', 'Migrations Start'));
   }
 
   up(name) {
     this.count++;
     console.log(
-      styleText('green', 'Migration: ') +
-        styleText('bold', this.count.toString()) +
-        ' ' +
-        styleText('green', 'up ') +
-        name
+      styleText('blueBright', 'ClickHouse ')
+        + styleText('green', 'Migration: ')
+        + styleText('bold', this.count.toString())
+        + ' '
+        + styleText('green', 'up ')
+        + name,
     );
   }
 
   done() {
-    console.log(styleText('green', 'Migrations Done'));
+    console.log(styleText('blueBright', 'ClickHouse ') + styleText('green', 'Migrations Done'));
   }
 }
