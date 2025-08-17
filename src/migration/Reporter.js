867a542d70d5c381130a53136b241c222b3ea20a
uasan <ua.san.alex@gmail.com>
2025-08-17 19:53:25 +0200

    Refactor reporter

--- src/migration/Reporter.js
+++ src/migration/Reporter.js
@@ -2,16 +2,17 @@ import { styleText } from 'node:util';
 
 export class Reporter {
   count = 0;
+  label = styleText('blueBright', 'ClickHouse ');
 
   star() {
-    console.log(styleText('blueBright', 'ClickHouse ') + styleText('green', 'Migrations Start'));
+    console.log(this.label + styleText('green', 'Migrations Start'));
   }
 
   up(name) {
     this.count++;
+
     console.log(
-      styleText('blueBright', 'ClickHouse ')
-        + styleText('green', 'Migration: ')
+      this.label + styleText('green', 'Migration: ')
         + styleText('bold', this.count.toString())
         + ' '
         + styleText('green', 'up ')
@@ -20,6 +21,6 @@ export class Reporter {
   }
 
   done() {
-    console.log(styleText('blueBright', 'ClickHouse ') + styleText('green', 'Migrations Done'));
+    console.log(this.label + styleText('green', 'Migrations Done'));
   }
 }
