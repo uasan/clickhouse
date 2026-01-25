9ec06a126becb48d7d144730da6728f45b64a214
uasan <ua.san.alex@gmail.com>
2026-01-25 17:31:54 +0100

    Implement exchange tables

--- src/models/ClickHouseTable.js
+++ src/models/ClickHouseTable.js
@@ -1,3 +1,5 @@
+import { randomUUID } from 'node:crypto';
+
 export class ClickHouseTable {
   static version = 1;
   static tableName = '';
@@ -8,5 +10,21 @@ export class ClickHouseTable {
 
   static toString() {
     return this.tableName;
+  }
+
+  static async exchange(query) {
+    const newTableName = query.client.sql('"' + randomUUID() + '"');
+
+    let sql = await query.client.sql`SHOW CREATE TABLE ${this}`.asValue();
+    sql = 'CREATE TABLE ' + newTableName + sql.slice(sql.indexOf('('));
+
+    try {
+      await query.client.sql`${query.client.sql(sql)} AS ${query}`;
+      await query.client.sql`EXCHANGE TABLES ${this} AND ${newTableName}`;
+    } finally {
+      await query.client.sql`DROP TABLE IF EXISTS ${newTableName}`;
+    }
+
+    console.log(sql);
   }
 }
---
+++ tests/exchange.js
@@ -0,0 +1,33 @@
+import { env } from 'node:process';
+import { ClickHouse, ClickHouseTable } from '../src/ClickHouse.js';
+
+const clickHouse = new ClickHouse({
+  url: env.CLICKHOUSE_URL,
+  username: env.CLICKHOUSE_USERNAME,
+  password: env.CLICKHOUSE_PASSWORD,
+});
+
+class MyModel extends ClickHouseTable {
+  static tableName = 'default.test_exchange';
+
+  async up({ clickHouse }) {
+    await clickHouse.sql`
+      CREATE OR REPLACE TABLE ${MyModel} (
+        id Int32
+      )
+      ENGINE = MergeTree
+      ORDER BY (id)`;
+  }
+
+  async down({ clickHouse }) {
+    await clickHouse.sql`
+      DROP TABLE IF EXISTS ${MyModel}
+    `;
+  }
+}
+
+await new MyModel().up({ clickHouse });
+
+await MyModel.exchange(clickHouse.sql`
+    SELECT 1  
+`);
